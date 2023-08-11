1cf644678ef75957012b417d43e9bb67f2ba5916
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-08-11 17:20:32 -0300

    Imagens de Logo Finalização do projeto

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import spring from '../images/springio-icon.svg'
+import react from '../images/reactjs-icon.svg'
+
 
 export default function Navbar() {
   return (
     <div>
       <nav class="navbar navbar-expand-lg nwvbar-dark bg-success">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Projeto Spring Boot & React</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
+          <a class="navbar-brand" href="/">
+          Projeto Spring Boot & React
+            <img src={spring} alt=""  class="ms-2"/>
+            <img src={react} alt="" class="rounded float-start me-2"/>
+            
+          </a>
           <Link class="btn btn-outline-light" to={"/addUser"}> + Usuario</Link>
-
         </div>
       </nav>
     </div>
